6d247663d76d9741f7f81875eb1164e2755e67ae
ddobr <ddobrose@yahoo.com>
2022-04-20 10:09:55 -0500

    first commit added routes and established connection to local host

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+
+app.get('/test', (req,res)=> {
+    res.send('network is working')
+})
+
+//routine controller
+app.route('/')
+.get((req,res)=>{
+    res.send('index of routines- includes view/edit/delete buttons by each routine, add routine button at bottom ')
+})
+.post((req,res)=>{
+    res.send('Create new Routine- add name, date,muscle groups worked')
+})
+
+//workout controller
+app.route('/:id/workouts')
+.get((req,res) =>{
+    res.send('Do/View a routine id includes forms to log actual reps done on each set, includes edit routine button and add workout button and completed checkbox')
+})
+.post((req,res)=>{
+    res.send('create a new workout for said routine id ')
+})
+.delete((req,res)=>{
+    res.send('delete a workout from routine by ID')
+})
+
+//routine Controller
+app.get('/edit/:id', (req,res)=>{
+    res.send('edit routine by ID Form- includes forms to add or change muscle groups worked and change date to complete and name has submit button')
+})
+
+app.get('/new', (req,res)=>{
+    res.send('Create a Routine Form- has all same info as edit')
+})
+
+
+
+//Workout Controller
+app.route('/:id/workouts/:id')
+.put((req,res)=>{
+    req.params.id
+    res.send('edit  routine by ID- edit name of routine and muscle groups, date, and completed checkbox')
+})
+
+app.get('/:id/workouts/new', (req,res)=>{
+    res.send('form to create workout same as edit a workout includes cancel and submit button')
+})
+
+
+
+
+
+const port = process.env.PORT || 3421
+
+app.listen(port, () => {
+    console.log(`Every Set app is running on ${port}`)
+})
