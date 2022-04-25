f7cca2e9cae4afa61d284f35b15dbd9b52f1bd26
ddobr <ddobrose@yahoo.com>
2022-04-25 14:04:14 -0500

    added more views and forms

--- controllers/routine-controller.js
+++ controllers/routine-controller.js
@@ -4,6 +4,7 @@ const res = require('express/lib/response')
 const router = express.Router()
 
 const Routine = require('../models/routine-model')
+const Workout = require('../models/workout-model')
 
 router.route('/')
 .get((req,res)=>{
@@ -32,7 +33,8 @@ router.get('/new', (req,res)=>{
     res.render('routines/routine-create-form')
 })
 
-router.put('/:id', (req,res)=>{
+router.route("/:id")
+.put((req,res)=>{
     const id = req.params.id
     Routine.findOneAndUpdate(
         {_id:id},
@@ -40,17 +42,72 @@ router.put('/:id', (req,res)=>{
             name: req.body.name, 
     dayToDo: req.body.dayToDo,
     muscleGroups: req.body.muscleGroups,
-    completed:req.body.completed,
+    completed:req.body.completed === "on",
 },
 {new:true}
-.then((routine)=>{
-    res.render('routines/routine-index', routine)
+    )
+.then(()=>{
+    res.redirect('/routines')
 })
 .catch(console.error)
-        
-    )
-    
 })
+.delete((req,res)=>{
+    const id = req.params.id
+    Workout.deleteMany({_routineId:id})
+   Routine.findOneAndRemove({_id:id})
+   .then(()=>{
+       res.redirect('/routines')
+   })
+   .catch(console.error)
+})
+
+router.route('/:id/workouts')
+.get((req,res) =>{
+    // Routine.find({_id:req.params.id}) // How do i declare this
+    Workout.find({_routineId:req.params.id})//and this as separate variables in order to reference both in the view page
+    .then((workouts)=>{
+        res.render('workouts/view-routine-workouts',{workouts})
+        console.log(workouts[0].routineId)//like this
+        console.log(workouts[0].id)
+})
+})
+.post((req,res)=>{
+    res.send('create a new workout for said routine id ')
+})
+
+
+
+//routine Controller
+
+
+
+
+//Workout Controller
+router.route('/:id/workouts/:workoutid/edit')
+.get((req,res)=>{
+    res.send('form to view/edit workout in routine by id')
+})
+
+
+
+router.route('/:id/workouts/:workoutid')
+.put((req,res)=>{
+    res.send(`edit workout ${req.params.workoutid}`)
+})
+.delete((req,res)=>{
+    res.send('delete a workout in routine by ID')
+})
+
+
+router.get('/:id/workouts/new', (req,res)=>{
+    Routine.findById(req.params.id)
+    .then((routine)=>{
+    res.render('workouts/create-new-workout',routine)
+    console.log(routine.name)
+})
+.catch(console.error)
+})
+
     
 
 
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const methodOverride = require('method-override');
 require('dotenv').config()
 const app = express()
 const routineController = require('./controllers/routine-controller')
-const workoutController = require('./controllers/workout-controller')
-
+// const workoutController = require('./controllers/workout-controller')
+const methodOverride = require('method-override');
 
 app.set('view engine', 'hbs');
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.use('/routines', routineController)
-app.use('/routines/:id/workouts', workoutController)
+// app.use('/routines/:id/workouts', workoutController)
 
 app.get('/test', (req,res)=> {
     res.send('network is working')
@@ -20,19 +20,19 @@ app.get('/test', (req,res)=> {
 
 
 //workout controller
-app.route('/:id/workouts')
-.get((req,res) =>{
-    res.send('Do/View a routine id includes forms to log actual reps done on each set, includes edit routine button and add workout button and completed checkbox')
-})
-.post((req,res)=>{
-    res.send('create a new workout for said routine id ')
-})
-.put((req,res)=>{
-    res.send('edit the routine by ID')
-})
-.delete((req,res)=>{
-    res.send('delete a routine by ID and all workouts associated')
-})
+// app.route('/:id/workouts')
+// .get((req,res) =>{
+//     res.send('Do/View a routine id includes forms to log actual reps done on each set, includes edit routine button and add workout button and completed checkbox')
+// })
+// .post((req,res)=>{
+//     res.send('create a new workout for said routine id ')
+// })
+// .put((req,res)=>{
+//     res.send('edit the routine by ID')
+// })
+// .delete((req,res)=>{
+//     res.send('delete a routine by ID and all workouts associated')
+// })
 
 
 //routine Controller
@@ -41,22 +41,22 @@ app.route('/:id/workouts')
 
 
 //Workout Controller
-app.route('/:id/workouts/:id')
-.get((req,res)=>{
-    res.send('form to view/edit workout in routine by id')
-})
-.put((req,res)=>{
-    req.params.id
-    res.send('edit workout by ID')
-})
-.delete((req,res)=>{
-    res.send('delete a workout in routine by ID')
-})
+// app.route('/:id/workouts/:id')
+// .get((req,res)=>{
+//     res.send('form to view/edit workout in routine by id')
+// })
+// .put((req,res)=>{
+//     req.params.id
+//     res.send('edit workout by ID')
+// })
+// .delete((req,res)=>{
+//     res.send('delete a workout in routine by ID')
+// })
 
 
-app.get('/:id/workouts/new', (req,res)=>{
-    res.send('form to create workout same as edit a workout includes cancel and submit button')
-})
+// app.get('/:id/workouts/new', (req,res)=>{
+//     res.send('form to create workout same as edit a workout includes cancel and submit button')
+// })
 
 
 
